f2e6bc91179d95b4d1b2b40a778c96c97482a2c2
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-09-11 07:43:45 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { VueLoaderPlugin } = require("vue-loader");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  output: {
+    publicPath: "http://localhost:8084/",
+  },
+
+  resolve: {
+    alias: {
+      Components: path.resolve(__dirname, "src/components/"),
+      Views: path.resolve(__dirname, "src/views/"),
+    },
+    // extensions: [".vue", "..."],
+    extensions: [".tsx", ".ts", ".vue", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    static: path.join(__dirname, "public"),
+    port: 8084,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
+
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      // Images
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      // Fonts and SVGs
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new VueLoaderPlugin(),
+    new ModuleFederationPlugin({
+      name: "module2",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+        // "./HelloWorld": "./src/components/HelloWorld.vue",
+      },
+      shared: require("./package.json").dependencies,
+    }),
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/assets", to: "assets" }],
+    }),
+  ],
+};
