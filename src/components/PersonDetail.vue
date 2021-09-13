c7827f0a86bfe6b5e34a590ce2614e3e385bb90c
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-09-13 07:39:43 +0200

    style

--- src/components/PersonDetail.vue
+++ src/components/PersonDetail.vue
@@ -43,7 +43,7 @@
         <span class="a-text-bold">Country : </span>{{ person.country }}
       </div>
       <div>
-        <button @click="gotoEditBasicData">EDIT</button>
+        <button @click="gotoEditBasicData" class="btn btn-primary">EDIT</button>
       </div>
     </template>
   </detail-section>
@@ -152,8 +152,8 @@ export default {
       }
     },
     gotoEditBasicData() {
-      this.$router.push({path: `/person/edit/${this.person.id}`})
-    }
+      this.$router.push({ path: `/person/edit/${this.person.id}` });
+    },
   },
   data() {
     return {
@@ -181,9 +181,38 @@ export default {
 };
 </script>
 
+// todo: where/how to share styles?
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .a-text-bold {
   font-weight: 700 !important;
+}
+
+.btn {
+  margin: 2px 5px 0 0;
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  text-align: center;
+  text-decoration: none;
+  vertical-align: middle;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+  background-color: transparent;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.btn-primary {
+  color: #fff;
+  background-color: #0d6efd;
+  border-color: #0d6efd;
 }
 </style>
